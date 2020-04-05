6271e3e52342d127c0ab3d33d2942ba245172799
Tarcisio Cardoso <tarcardoso@gmail.com>
2020-04-05 10:11:04 -0300

    API para busca produções do user

---
+++ back-end/src/main/java/br/com/farm/adm/controller/producao/UserProductionController.java
@@ -0,0 +1,30 @@
+package br.com.farm.adm.controller.producao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.farm.adm.model.UserProduction;
+import br.com.farm.adm.service.UserProductionService;
+
+@RestController
+@RequestMapping("/api/userProduction")
+public class UserProductionController {
+
+    @Autowired
+    UserProductionService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<UserProduction> findById(@PathVariable("id") String id) {
+        UserProduction e = service.findById(id);
+        HttpStatus status = e != null ? HttpStatus.OK : HttpStatus.NOT_FOUND;
+        return new ResponseEntity<UserProduction>(e, status);
+    }
+
+}
