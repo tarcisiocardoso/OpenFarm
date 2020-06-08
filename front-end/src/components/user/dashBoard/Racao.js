8ec0136ca7209f04e384f926fa4353d25908bd77
Tarcisio Cardoso <tarcardoso@gmail.com>
2020-06-08 17:43:16 -0300

    Calculo para produção de ração.

--- front-end/src/components/user/dashBoard/Racao.js
+++ front-end/src/components/user/dashBoard/Racao.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Paper, TextField, InputAdornment, Typography } from '@material-ui/core';
+import { Grid, Paper, TextField, InputAdornment, Typography, Button } from '@material-ui/core';
 
 import { Radar } from 'react-chartjs-2';
 
@@ -34,8 +34,8 @@ export default function Racao() {
 
     const titulo = 'Formulação de ração';
 
-    const data = {
-        labels: ['Running', 'Swimming', 'Eating', 'Cycling'],
+    const [data, setData] = useState({
+        labels: [], //['Running', 'Swimming', 'Eating', 'Cycling'],
         datasets: [{
             label: 'Ração',
             backgroundColor: 'rgba(255,99,132,0.2)',
@@ -43,38 +43,38 @@ export default function Racao() {
             borderWidth: 1,
             hoverBackgroundColor: 'rgba(255,99,132,0.4)',
             hoverBorderColor: 'rgba(255,99,132,1)',
-            data: [20, 10, 4, 2]
+            data: [] //[20, 10, 4, 2]
         }]
-    }
-    // let arr ={};
-    // concentrado.forEach(item => {
-    //     arr[item.id]=50;
-    // });
+    });
 
-    // console.log('>>>arr<<<', arr);
-    // setItens(arr);
     const [itens, setItens] = useState(); //(arr);
     const [itensErro, setItensErro] = useState();
     const [proteina, setProteina] = useState(16);
 
     useEffect(() => {
-        console.log('>>>RACAO<<<');
         if (!itens) {
             calculaRacaoInicial();
         }
     }, [concentrado]);
 
     const handleChangeValor = (e, item) => {
-        console.log(e.target.value, "-->", item);
-        recalcula(e.target.value, item);
+        //recalcula(e.target.value, item);
+        let valor = e.target.value;
+        valor = valor?valor:0;
+        var arr = { ...itens };
+        arr[item.id] = parseFloat(valor);
+        setItens(arr);
+    }
+    const handleLostFocus = (e) =>{
+        console.log('>>>handleLostFocus<<<');
     }
     const handleProteina = (e) => {
         setProteina(e.target.value);
     }
 
     function calculaRacaoInicial() {
-        arrEnergeticos=[];
-        arrProteicos=[];
+        arrEnergeticos = [];
+        arrProteicos = [];
         concentrado.forEach(item => {
             if (item.p < proteina) {
                 arrEnergeticos.push(item);
@@ -84,7 +84,7 @@ export default function Racao() {
         });
         let protEnergetico = getValorProteico(arrEnergeticos);
         let protProteico = getValorProteico(arrProteicos);
-        
+
         let parteEnergetico = Math.abs(protProteico - proteina);
         let parteProteico = Math.abs(protEnergetico - proteina);
         let parteTotal = parteProteico + parteEnergetico;
@@ -98,90 +98,136 @@ export default function Racao() {
         let arr = {};
         let arrErro = {};
         arrEnergeticos.forEach(item => {
-            arr[item.id] = parseFloat((porceEnergetico/arrEnergeticos.length).toFixed(2));
-            arrErro[item.id]= '';
+            arr[item.id] = parseFloat((porceEnergetico / arrEnergeticos.length).toFixed(2));
+            arrErro[item.id] = '';
         });
         arrProteicos.forEach(item => {
-            arr[item.id] = parseFloat((porceProteico/arrProteicos.length).toFixed(2));
-            arrErro[item.id]= '';
+            arr[item.id] = parseFloat((porceProteico / arrProteicos.length).toFixed(2));
+            arrErro[item.id] = '';
         });
         console.log('>>>arr<<<', arr);
+
+        let dado = {...data};
+        dado.labels=[];
+        dado.datasets[0].data=[];
+        for(let x in arr){
+            if( arr[x] > 0 ){
+                dado.labels.push(x);
+                dado.datasets[0].data.push( arr[x])
+            }
+        }
+        setData(dado);
+
         setItens(arr);
         setItensErro(arrErro);
     }
-    function getPorcentagem(id){
+    function getValorProteicoRacao(id) {
         let val = 0;
-        for(let x in concentrado){
-            if( concentrado[x].id === id){
-                val = concentrado[x].p/100;
+        for (let x in concentrado) {
+            if (concentrado[x].id === id) {
+                val = concentrado[x].p;
+                break;
             }
         }
         return val;
     }
-    function getValorProteico(arr, item) {
-        if( !item){
-            let soma = 0;
-            arr.forEach(it=> {
-                soma += it.p;
-            })
-            return soma / arr.length;
-        }else{
-            let totalParte=0;
-            arr.forEach(it=> {
-                totalParte += parseFloat(itens[it.id]);
-            })
-            let qtd=0;
-            arr.forEach(it => {
-                qtd += (it.p * itens[it.id]/totalParte);
-                console.log( it.id+' --->', (it.p * itens[it.id]/totalParte) );
-            })
-            return qtd;
-        }
+    function getValorProteico(arr) {
+        let soma = 0;
+        arr.forEach(it => {
+            soma += it.p;
+        })
+        return soma / arr.length;
     }
-    function recalcula(valor, item){
-        var arr = {...itens};
-        var arrErro = {...itensErro};
-
-        if( item.limiteEmPC && item.limiteEmPC < valor){
-            arrErro[item.id] = "Ultramassou limite indicado!";
-        }else{
-            arrErro[item.id] = "";
-        }
-
-       let isRecalculando = true;
-       let qtd = 0;
-       let acrescimo = (valor > arr[item.id])?-1:1;
-       let totalProt = 0;
-       while( isRecalculando ){
-        // console.log('>>>recalculando<<<', acrescimo);
-        totalProt = 0;
-        for(let x in arr){
-            // console.log(x, '-->', arr[x]);
-            
-            if( x !== item.id){
-                totalProt += (arr[x] * getPorcentagem(x) );
-                if( arr[x]> 0 ){
-
-                }
-            }else{
-                totalProt += (valor * getPorcentagem(x) );
-            }
-            if( totalProt > (proteina + 0.50)){
-
-            }else if( totalProt < proteina ){
-
-            }else{
-                isRecalculando = false;// montar os parametros concluido
-            }
-        }
-
-        if( qtd++ > 2) break;
-       }
-       console.log("totalProt: "+ totalProt );
-       arr[item.id] = valor;
-       setItens(arr);
-       setItensErro(arrErro);
+    function getValorProteicoEmPc(arr){
+        let soma = 0;
+        let total =0;
+        arr.forEach(it => {
+            total += it.parte;
+        }) 
+        arr.forEach(it => {
+            soma += it.proteina * (it.parte/total);
+        })
+        return soma;
+    }
+    function handleOnClick(e){
+        console.log('>>handleOnClick<<');
+        recalcula();
+    }
+    function recalcula() {
+        var arr = { ...itens };
         
+        let total = 0;
+        Object.entries(arr).map( i => total += i[1]);
+
+        let novaRacao= [];
+        for(let x in arr){
+            novaRacao.push({
+                nome:x,
+                parte:arr[x],
+                proteina: getValorProteicoRacao(x),
+            });
+        }
+        arrEnergeticos = [];
+        arrProteicos = [];
+        novaRacao.forEach(item => {
+            if (item.proteina < proteina) {
+                arrEnergeticos.push(item);
+            } else {
+                arrProteicos.push(item);
+            }
+        });
+        let protEnergetico = getValorProteicoEmPc(arrEnergeticos);
+        let protProteico = getValorProteicoEmPc(arrProteicos);
+
+        let parteEnergetico = Math.abs(protProteico - proteina);
+        let parteProteico = Math.abs(protEnergetico - proteina);
+        let parteTotal = parteProteico + parteEnergetico;
+
+        let porceEnergetico = (parteEnergetico / parteTotal) * 100;
+        let porceProteico = (parteProteico / parteTotal) * 100;
+
+        let totalParte=0;
+        arrEnergeticos.forEach( it => totalParte += it.parte);
+
+        arrEnergeticos.forEach(item => {
+            arr[item.nome] = parseFloat((porceEnergetico * (item.parte/totalParte)).toFixed(2));
+            // arrErro[item.nome] = '';
+        });
+        totalParte=0;
+        arrProteicos.forEach( it => totalParte += it.parte);
+        arrProteicos.forEach(item => {
+            arr[item.nome] = parseFloat((porceProteico * (item.parte/totalParte)).toFixed(2));
+            // arrErro[item.nome] = '';
+        });
+
+        let dado = {...data};
+        dado.labels=[];
+        dado.datasets[0].data=[];
+        for(let x in arr){
+            if( arr[x] > 0 ){
+                dado.labels.push(x);
+                dado.datasets[0].data.push( arr[x])
+            }
+        }
+
+        validarRacao(arr);
+
+        setData(dado);
+
+        setItens(arr);
+    }
+    function validarRacao(arr){
+        var arrErro = { ...itensErro };
+        for(let x in arr){
+            let item = concentrado.find(item => item.id === x);
+            if( item && item.limiteEmPC && arr[x] > item.limiteEmPC  ){
+                arrErro[x] = "Ultramassou limite indicado!"; 
+            }else{
+                arrErro[x] = "";
+            }
+        }
+
+        setItensErro(arrErro);
     }
     return (
         <Paper>
@@ -215,12 +261,16 @@ export default function Racao() {
                                     endAdornment: <InputAdornment position="end">{item.p + "%"}</InputAdornment>,
                                 }}
                                 onChange={e => { handleChangeValor(e, item) }}
+                                onBlur={e => handleLostFocus(e, item)}
                                 value={itens[item.id]}
                                 variant="filled"
                                 helperText={itensErro[item.id]}
                             />
                         ))
                         }
+                        <Button variant="contained" color="primary" onClick={handleOnClick}>
+                            Recalcular
+                        </Button>
                         <Typography variant="subtitle1">
                             Porcentagem proteica
                         </Typography>
